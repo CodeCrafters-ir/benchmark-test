eb5a26c17b952be4c4219e31f32c52c9f93e62a9
behzad azadi <behzad.azadi2693@gmail.com>
2024-03-07 13:31:00 +0330

    benchmark test in python

--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -38,3 +38,20 @@ def test_quick_sort_large(sort_input2, benchmark):
 def test_insertion_sort_large(sort_input2, benchmark):
     result = benchmark(insertion_sort, sort_input2)
     assert result == sorted(sort_input2)
+
+
+
+# Function to benchmark  
+def expensive_operation():  
+    total = 0  
+    for i in range(1000000):  
+        total += i  
+    return total  
+  
+# Benchmarking test  
+@pytest.mark.benchmark  
+def test_expensive_operation(benchmark):  
+    result = benchmark(expensive_operation)  
+    assert result
+
+
